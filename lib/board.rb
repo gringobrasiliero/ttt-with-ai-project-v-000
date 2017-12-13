dd7b7297fb56d7dd73b6ff0d91afe74c34e905a0
Nolan Byrnes <nolanehs@gmail.com>
2017-12-13 16:55:00 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -28,7 +28,7 @@ def full?
 
 
   def taken?(input)
-       !(position(input) == @token || position(input) == "")
+       !(position(input) == ("X" || "O") || position(input) == "")
      end
 
 def turn_count
